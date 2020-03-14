df26da0f15148a16d27ca9bbfd9f5292ba709f2f
parsalotfy <parsalotfy@outlook.com>
2020-03-15 02:00:26 +0330

    Get API data of site suggestion

--- DeutschHelp2/Form1.cs
+++ DeutschHelp2/Form1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using HtmlAgilityPack;
@@ -77,5 +79,71 @@ namespace DeutschHelp2
         {
             new Show(words).Show();
         }
+
+
+        public List<Suggestion> GetData()
+        {
+            // Load Page via Get Request
+            HttpWebRequest getRequest = (HttpWebRequest)WebRequest.Create("https://wort.ir");
+
+            // Get Cookies
+            getRequest.CookieContainer = new CookieContainer();
+            HttpWebResponse getResponse = (HttpWebResponse)getRequest.GetResponse();
+
+            // Get Token
+            string getResponseString = new StreamReader(getResponse.GetResponseStream()).ReadToEnd();
+            string tokenPattern = "<body data-vtn=\"(\\w+)\"";
+            string getToken = Regex.Match(getResponseString, tokenPattern).Groups[1].Value;
+
+            // Generate Body
+            string word = "buc";
+            string postData = $"search={word}&_token={getToken}";
+            byte[] data = Encoding.ASCII.GetBytes(postData);
+
+            // Generate Post Request
+            HttpWebRequest postRequest = (HttpWebRequest)WebRequest.Create("https://wort.ir/woerterbuch/ac");
+
+            // Add Cookies
+            postRequest.CookieContainer = new CookieContainer();
+            postRequest.CookieContainer.Add(getResponse.Cookies);
+
+            // Add Headers
+            postRequest.Headers["authority"] = "wort.ir";
+            postRequest.Method = "POST";
+            postRequest.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+            postRequest.ContentLength = data.Length;
+
+            // Send Post Stream
+            using (var stream = postRequest.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+
+            }
+
+            // Get Response
+            HttpWebResponse postResponse = (HttpWebResponse)postRequest.GetResponse();
+            string postResponseString = new StreamReader(postResponse.GetResponseStream()).ReadToEnd();
+
+            // Release Resources
+            getResponse.Close();
+            postResponse.Close();
+
+            // Deserialize response
+            List<Suggestion> postResult =JsonSerializer.Deserialize<List<Suggestion>>(postResponseString);
+
+            return postResult;
+        }
+
+
     }
+
+
+
+    public class Suggestion
+    {
+        public string label { get; set; }
+        public string type_string { get; set; }
+        public string full_slug { get; set; }
+    }
+
 }
