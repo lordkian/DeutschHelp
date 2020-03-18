065b7244575ab9663221db8ada060d350665a6ad
kian kazem <lordkian2000@hotmail.com>
2020-03-18 23:45:33 +0330

    making download multi thread

--- DeutschHelp/Form1.cs
+++ DeutschHelp/Form1.cs
@@ -6,52 +6,100 @@ using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DeutschHelp
 {
     public partial class Form1 : Form
     {
-        List<Word> words = new List<Word>();
+        List<Word> word = new List<Word>();
         List<WordPackung> wordPackungen = new List<WordPackung>();
         SuggestionCrawler suggestionCrawler = new SuggestionCrawler();
+        Task task;
+        CancellationTokenSource tokenSource2;
         public Form1()
         {
             InitializeComponent();
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            words.Clear();
-            wordPackungen.Clear();
             var str = Regex.Replace(textBox1.Text, "\\s+", " ").Replace("\r", "").Replace(".", "").Replace(",", "");
             var strs = str.Split(' ').Where(d => d.Length > 0).Distinct().ToList();
-            int i = 0;
-            suggestionCrawler.GetCookie();
-            foreach (var item in strs)
+            if (strs.Count == 0)
             {
-                var slist = suggestionCrawler.GetSuggestions(item);
-                var wlist = new List<Word>();
-                foreach (var item2 in slist)
-                {
-                    var url = "https://wort.ir" + item2.full_slug;
-                    HtmlWeb htmlWeb = new HtmlWeb();
-                    var html = htmlWeb.Load(url);
-                    try
-                    {
-                        wlist.Add(html.DocumentNode.GetEncapsulatedData<Word>());
-                    }
-                    catch (Exception) { }
-                }
-                if (wlist.Count > 0)
-                    wordPackungen.Add(new WordPackung() { Text = item, Words = wlist });
-                if (wlist.Count > 0)
-                    words.Add(Word.Merge(wlist));
-                i++;
-                progressBar1.Value = 100 * i / strs.Count;
+                MessageBox.Show("There is no word to show.");
+                return;
             }
-        }
 
+            wordPackungen.Clear();
+
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button1.Text = "Stop";
+            button1.Click -= button1_Click;
+            button1.Click += button1_Click2;
+
+            tokenSource2 = new CancellationTokenSource();
+            CancellationToken ct = tokenSource2.Token;
+            task = Task.Run(() =>
+             {
+                 var str = textBox1.Text.Replace(".", " ").Replace(",", " ");
+                 str = Regex.Replace(str, "\\s+", " ").Replace("\r", "");
+                 var strs = str.Split(' ').Where(d => d.Length > 0).Distinct().ToList();
+                 int i = 0;
+                 suggestionCrawler.GetCookie();
+                 foreach (var item in strs)
+                 {
+                     if (ct.IsCancellationRequested)
+                     {
+                         Invoke(new Action(Finish));
+                         ct.ThrowIfCancellationRequested();
+                         return;
+                     }
+                     try
+                     {
+                         var slist = suggestionCrawler.GetSuggestions(item);
+                         var wlist = new List<Word>();
+                         foreach (var item2 in slist)
+                         {
+                             var url = "https://wort.ir" + item2.full_slug;
+                             HtmlWeb htmlWeb = new HtmlWeb();
+                             var html = htmlWeb.Load(url);
+                             try
+                             {
+                                 wlist.Add(html.DocumentNode.GetEncapsulatedData<Word>());
+                             }
+                             catch (Exception) { }
+                         }
+                         if (wlist.Count > 0)
+                             wordPackungen.Add(new WordPackung() { Text = item, Words = wlist });
+                     }
+                     catch (Exception e)
+                     {
+                         int j = 0;
+                     }
+                     i++;
+                     ChangeProgressBarPercentage(100 * i / strs.Count);
+                 }
+                 Invoke(new Action(Finish));
+             }, tokenSource2.Token);
+        }
+        private void button1_Click2(object sender, EventArgs e)
+        {
+            tokenSource2.Cancel();
+        }
+        public void Finish()
+        {
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button1.Text = "Download";
+            button1.Click -= button1_Click2;
+            button1.Click += button1_Click;
+        }
         private void button3_Click(object sender, EventArgs e)
         {
             if (wordPackungen.Count == 0)
@@ -72,7 +120,6 @@ namespace DeutschHelp
             sw.WriteLine(JsonConvert.SerializeObject(new Serializable() { Text = textBox1.Text, WordPackungen = wordPackungen, Version = 1.2 }, Formatting.Indented));
             sw.Close();
         }
-
         private void button4_Click(object sender, EventArgs e)
         {
             var ofd = new OpenFileDialog()
@@ -85,7 +132,7 @@ namespace DeutschHelp
             if (res == DialogResult.Cancel)
                 return;
             var sr = new StreamReader(ofd.FileName);
-            words.Clear();
+            wordPackungen.Clear();
             textBox1.Text = "";
             var str = sr.ReadToEnd();
             sr.Close();
@@ -111,7 +158,6 @@ namespace DeutschHelp
                     wordPackungen.Add(new WordPackung() { Text = item.Text, Words = new List<Word>() { item } });
             }
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
             if (wordPackungen.Count == 0)
@@ -121,7 +167,6 @@ namespace DeutschHelp
             }
             new Show(wordPackungen).Show();
         }
-
         public void ChangeProgressBarPercentage(int percentage)
         {
             progressBar1.Invoke(new Action(() => { progressBar1.Value = percentage; }));
