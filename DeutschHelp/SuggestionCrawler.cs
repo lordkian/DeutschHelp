4461a4eb8c89b9949374e9bcfdf766e4d8939499
kian kazem <lordkian2000@hotmail.com>
2020-03-18 23:50:51 +0330

    fixing ObjectDisposedException error

--- DeutschHelp/SuggestionCrawler.cs
+++ DeutschHelp/SuggestionCrawler.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
@@ -31,40 +32,48 @@ namespace DeutschHelp
         }
         public List<Suggestion> GetSuggestions(string word)
         {
-            string postData = $"search={word}&_token={getToken}";
-            byte[] data = Encoding.ASCII.GetBytes(postData);
-
-            // Generate Post Request
-            HttpWebRequest postRequest = (HttpWebRequest)WebRequest.Create("https://wort.ir/woerterbuch/ac");
-
-            // Add Cookies
-            postRequest.CookieContainer = new CookieContainer();
-            postRequest.CookieContainer.Add(getResponse.Cookies);
-
-            // Add Headers
-            postRequest.Headers["authority"] = "wort.ir";
-            postRequest.Method = "POST";
-            postRequest.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
-            postRequest.ContentLength = data.Length;
-
-            // Send Post Stream
-            using (var stream = postRequest.GetRequestStream())
+            try
             {
-                stream.Write(data, 0, data.Length);
+                string postData = $"search={word}&_token={getToken}";
+                byte[] data = Encoding.ASCII.GetBytes(postData);
+
+                // Generate Post Request
+                HttpWebRequest postRequest = (HttpWebRequest)WebRequest.Create("https://wort.ir/woerterbuch/ac");
+
+                // Add Cookies
+                postRequest.CookieContainer = new CookieContainer();
+                postRequest.CookieContainer.Add(getResponse.Cookies);
+
+                // Add Headers
+                postRequest.Headers["authority"] = "wort.ir";
+                postRequest.Method = "POST";
+                postRequest.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+                postRequest.ContentLength = data.Length;
+
+                // Send Post Stream
+                using (var stream = postRequest.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+
+                // Get Response
+                HttpWebResponse postResponse = (HttpWebResponse)postRequest.GetResponse();
+                string postResponseString = new StreamReader(postResponse.GetResponseStream()).ReadToEnd();
+
+                // Release Resources
+                getResponse.Close();
+                postResponse.Close();
+
+                // Deserialize response
+                List<Suggestion> postResult = JsonConvert.DeserializeObject<List<Suggestion>>(postResponseString);//serializer.Deserialize<List<Suggestion>>(postResponseString);
+
+                return postResult;
             }
-
-            // Get Response
-            HttpWebResponse postResponse = (HttpWebResponse)postRequest.GetResponse();
-            string postResponseString = new StreamReader(postResponse.GetResponseStream()).ReadToEnd();
-
-            // Release Resources
-            getResponse.Close();
-            postResponse.Close();
-
-            // Deserialize response
-            List<Suggestion> postResult = JsonConvert.DeserializeObject<List<Suggestion>>(postResponseString);//serializer.Deserialize<List<Suggestion>>(postResponseString);
-
-            return postResult;
+            catch (ObjectDisposedException)
+            {
+                GetCookie();
+                return GetSuggestions(word);
+            }
         }
     }
 }
